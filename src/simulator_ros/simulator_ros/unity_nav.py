3276307e992598e5132420e1e1b6c4fb6598f774
calebchia1807 <calebchia1807@gmail.com>
2025-04-16 18:30:17 +0800

    original

--- src/simulator_ros/simulator_ros/unity_nav.py
+++ src/simulator_ros/simulator_ros/unity_nav.py
@@ -55,37 +55,42 @@ class UnityNav(Node):
         angular_y = msg.angular.y   # look up / down - pitch
         angular_z = msg.angular.z   # rotate left / right - yaw
         
+        commands = []
         timestep = 0.02
         
-        # no motion
-        if linear_x == 0 and linear_y == 0 and linear_z == 0 and angular_x == 0 and angular_y == 0 and angular_z == 0:
-            self.send_command_to_unity(json.dumps({"action": "MoveAhead", "moveMagnitude": 0}))
-            self.send_command_to_unity(json.dumps({"action": "MoveLeft", "moveMagnitude": 0}))
-            self.send_command_to_unity(json.dumps({"action": "RotateLeft", "degrees": 0}))
-
         # move forward & backward
         if linear_x != 0:
             velocity_x = abs(linear_x)
             move_magnitude_x = velocity_x * timestep
+            steps_linear_x = int(velocity_x / move_magnitude_x)
 
             if linear_x > 0:
                 self.get_logger().info(f"Moving Ahead @ {velocity_x} m/s")
-                self.send_command_to_unity(json.dumps({"action": "MoveAhead", "moveMagnitude": move_magnitude_x}))
             elif linear_x < 0:
                 self.get_logger().info(f"Moving Back @ {velocity_x} m/s")
-                self.send_command_to_unity(json.dumps({"action": "MoveBack", "moveMagnitude": move_magnitude_x}))
+    
+            for _ in range(steps_linear_x):
+                if linear_x > 0:
+                    commands.append({"action": "MoveAhead", "moveMagnitude": move_magnitude_x})
+                elif linear_x < 0:
+                    commands.append({"action": "MoveBack", "moveMagnitude": move_magnitude_x})
 
         # move left & right
         if linear_y != 0:
             velocity_y = abs(linear_y)
             move_magnitude_y = velocity_y * timestep
+            steps_linear_y = int(velocity_y / move_magnitude_y)
             
             if linear_y > 0:
                 self.get_logger().info(f"Moving Left @ {velocity_y} m/s")
-                self.send_command_to_unity(json.dumps({"action": "MoveLeft", "moveMagnitude": move_magnitude_y}))
             elif linear_y < 0:
                 self.get_logger().info(f"Moving Right @ {velocity_y} m/s")
-                self.send_command_to_unity(json.dumps({"action": "MoveRight", "moveMagnitude": move_magnitude_y}))
+            
+            for _ in range(steps_linear_y):
+                if linear_y > 0:
+                    commands.append({"action": "MoveLeft", "moveMagnitude": move_magnitude_y})
+                elif linear_y < 0:
+                    commands.append({"action": "MoveRight", "moveMagnitude": move_magnitude_y})
 
         # stand & crouch
         if linear_z != 0:
@@ -93,40 +98,56 @@ class UnityNav(Node):
             
             if linear_z > 0:
                 self.get_logger().info(f"Standing")
-                self.send_command_to_unity(json.dumps({"action": "Stand"}))
+                commands.append({"action": "Stand"})
             elif linear_z < 0:
                 self.get_logger().info(f"Crouching")
-                self.send_command_to_unity(json.dumps({"action": "Crouch"}))
+                commands.append({"action": "Crouch"})
 
         # look up & down
         if angular_y != 0:
             # angle_velocity_x = abs(math.degrees(angular_x))    # angular_x in radians - conversion needed as the command sent need to be in degrees!!!
             angle_velocity_y = abs(angular_y)                    # angular_x in degrees
             rotate_magnitude_y = angle_velocity_y * timestep 
+            steps_angular_y = int(angle_velocity_y / rotate_magnitude_y)
 
             if angular_y > 0:
                 self.get_logger().info(f"Looking up @ {angle_velocity_y} degrees/s")
                 #self.get_logger().info(f"Looking up @ {abs(angular_x)} rad/s")
-                self.send_command_to_unity(json.dumps({"action": "LookUp", "degrees": rotate_magnitude_y}))
             elif angular_y < 0:
                 self.get_logger().info(f"Looking down @ {angle_velocity_y} degrees/s")
                 #self.get_logger().info(f"Looking down @ {abs(angular_x)} rad/s")
-                self.send_command_to_unity(json.dumps({"action": "LookDown", "degrees": rotate_magnitude_y}))
+
+
+            for _ in range(steps_angular_y):
+                if angular_y > 0:
+                    commands.append({"action": "LookUp", "degrees": rotate_magnitude_y})
+                elif angular_y < 0:
+                    commands.append({"action": "LookDown", "degrees": rotate_magnitude_y})
 
         # rotate left & right (range btw -30 degrees to 30 degrees only)
         if angular_z != 0:
             # angle_velocity_z = abs(math.degrees(angular_z))    # angular_z in radians
             angle_velocity_z = abs(angular_z)                    # angular_z in degrees
             rotate_magnitude_z = angle_velocity_z * timestep 
+            steps_angular_z = int(angle_velocity_z / rotate_magnitude_z)
 
             if angular_z > 0:
                 self.get_logger().info(f"Rotating Left @ {angle_velocity_z} degrees/s")
                 #self.get_logger().info(f"Rotating Left @ {abs(angular_z)} rad/s")
-                self.send_command_to_unity(json.dumps({"action": "RotateLeft", "degrees": rotate_magnitude_z}))
             elif angular_z < 0:
                 self.get_logger().info(f"Rotating Right @ {angle_velocity_z} degrees/s")
                 #self.get_logger().info(f"Rotating Right @ {abs(angular_z)} rad/s")
-                self.send_command_to_unity(json.dumps({"action": "RotateRight", "degrees": rotate_magnitude_z}))
+
+            for _ in range(steps_angular_z):
+                if angular_z > 0:
+                    commands.append({"action": "RotateLeft", "degrees": rotate_magnitude_z})
+                elif angular_z < 0:
+                    commands.append({"action": "RotateRight", "degrees": rotate_magnitude_z})
+
+        if commands:
+            for command in commands:
+                command_json = json.dumps(command)
+                self.send_command_to_unity(command_json)
 
     def send_command_to_unity(self, message):
         try:
@@ -136,7 +157,6 @@ class UnityNav(Node):
             client_socket.close()
         except ConnectionRefusedError:
             self.get_logger().error("Failed to connect to Unity server!")
-
 
 def main(args=None):
     rclpy.init(args=args)
